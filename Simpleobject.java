8eec28f268e41883d53c6890a0f0d7b7b46f557a
Shivam Gupta <71491695+G5shivam@users.noreply.github.com>
2023-04-20 13:04:51 -0400

    Add files via upload

---
+++ Simpleobject.java
@@ -0,0 +1,9 @@
+public class Simpleobject {
+    Simpleobject(){
+        System.out.println("constructor is being called of class");
+    }
+
+public static void main(String [] args){
+    Simpleobject Simpleobject = new Simpleobject(); 
+   }
+}
