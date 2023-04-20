ac1addc33facf0fd473bc9c3fd7fab94ef60014d
Shivam Gupta <71491695+G5shivam@users.noreply.github.com>
2023-04-21 01:21:26 +0530

    Add files via upload

---
+++ SumofNumber.java
@@ -0,0 +1,16 @@
+public class SumofNumber{
+    public static void main(String [] args)
+    {
+        int i,num=11;
+        int sum =0;
+        for(i=1; i<num;++i)
+        /*{
+            System.out.println(i);
+            
+        }*/
+      { 
+         sum = sum + i;
+      }
+           System.out.println(sum);
+    }
+} 
