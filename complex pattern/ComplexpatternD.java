16165526d79bafec44e7bfa6b16d507f230bf47e
Shivam Gupta <71491695+G5shivam@users.noreply.github.com>
2023-04-24 01:32:48 +0530

    Add files via upload

---
+++ complex pattern/ComplexpatternD.java
@@ -0,0 +1,24 @@
+package pattern;
+
+public class ComplexpatternD {
+    public static void main(String[] args) {
+        int n=8;
+        for(int i = 0;i<n; i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                //if(i==j || i+j==n-1 || i==0 || j==0 || i==n-1 || j==n-1) //for x
+               //if(i+j==n-6 || i==j-5 || i==5) //for triangle
+              //if(i==0||j==0||i==n-1||j==n-1||i+j==n-7||i==j-6||i+j==n+5||i==j+6||j==7 ) //for sand clock
+              if(i==0 && j<(n-1)/2||j==0||i==n-1 &&j<(n-1)/2||j==(n-1)/2 && i>0 && i<n-1) //for D
+                { 
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }System.out.println(" ");
+        }
+        
+    }
+    
+}
