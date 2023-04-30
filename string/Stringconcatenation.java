7981455b29c61655ada3759f4d1ccd3dbb14ec52
Shivam Gupta <71491695+G5shivam@users.noreply.github.com>
2023-04-30 23:05:36 +0530

    Add files via upload

---
+++ string/Stringconcatenation.java
@@ -0,0 +1,19 @@
+package string;
+
+public class Stringconcatenation{
+    
+
+    public static void main(String[] args) {
+
+        String brand = "Shivam";
+        System.out.println(brand);
+
+        StringBuilder brand1 = new StringBuilder("Shivam");
+        System.out.println(brand1);
+        brand1.append(" Gupta");
+        System.out.println(brand1);
+        
+
+        
+    }
+}
