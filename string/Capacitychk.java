38b31c861e7b04c214d1d682cfd463a9e42798d0
Shivam Gupta <71491695+G5shivam@users.noreply.github.com>
2023-05-04 23:58:05 +0530

    Add files via upload

---
+++ string/Capacitychk.java
@@ -0,0 +1,15 @@
+public class Capacitychk {
+    
+    
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        System.out.println(sb.capacity());
+        sb.append = ("abcdefghijklmnopqrs");
+        
+        sb.append("s");
+        System.out.println(sb.capacity());
+        
+      
+        
+    }
+}
