c682c40811aaf57890715f544e65250daf8f6c74
Shivam Gupta <71491695+G5shivam@users.noreply.github.com>
2023-05-08 19:10:04 +0530

    Add files via upload

---
+++ string/Constructor.java
@@ -0,0 +1,35 @@
+
+
+class Student{
+    private String name;
+    private int age;
+
+    Student(String name , int age ){
+        this.name=name;
+        this.age=age;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public int getAge(){
+        return age;
+    }
+}
+
+public class Constructor {
+    public static void main(String[] args) {
+        Student st= new Student("shivam", 22);
+         
+
+        String name=st.getName();
+        System.out.println(name);
+
+        int age=st.getAge();
+        System.out.println(age);
+        
+
+    }
+
+   
+}
