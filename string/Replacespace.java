0fd616e46c761f3544bfc4656000a1b53e273740
Shivam Gupta <71491695+G5shivam@users.noreply.github.com>
2023-05-20 23:58:31 +0530

    Add files via upload

---
+++ string/Replacespace.java
@@ -0,0 +1,19 @@
+package string;
+
+public class Replacespace {
+    public static void main(String[] args) {
+        String str="hello I am learning java";
+        char ch='*';
+     
+
+
+       str=str.replace(' ',ch);
+
+       System.out.println("string after replace with character");
+       System.out.println(str);
+
+        
+        
+
+    }
+}
