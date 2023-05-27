6c0b5bb6954e9366f668a9244b7d52e3de1ac663
Shivam Gupta <71491695+G5shivam@users.noreply.github.com>
2023-05-27 23:59:55 +0530

    Add files via upload

---
+++ complex pattern/Halfdiamond.java
@@ -0,0 +1,22 @@
+package pattern;
+
+import java.util.Scanner;
+public class Halfdiamond {
+
+ public static void main(String[] args) {
+  Scanner cs=new Scanner(System.in);
+
+     System.out.println("Enter the row size:");
+     int row_size=cs.nextInt();
+     for(int out=row_size;out>=-row_size;out--)
+     {
+      for(int in=1;in<=Math.abs(out);in++)
+       System.out.printf(" ");
+     for(int p=row_size;p>=Math.abs(out);p--)
+      System.out.printf("*");
+     System.out.println();
+     }
+     cs.close();
+ }
+
+}
