4be82510a5387cbc08a24c0c2650b224eddcb8c7
Shivam Gupta <71491695+G5shivam@users.noreply.github.com>
2023-06-06 21:52:29 +0530

    Add files via upload

---
+++ File Handling/Copyfile.java
@@ -0,0 +1,27 @@
+package File_Handling;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+public class Copyfile {
+   public static void main(String[] args) throws IOException {
+      //Creating a File object to hold the source file
+      File source = new File("skill.txt");
+      //Creating a File object to hold the destination file
+      File destination = new File("new.txt");
+      //Creating an FileInputStream object
+      FileInputStream inputStream = new FileInputStream(source);
+      //Creating an FileOutputStream object
+      FileOutputStream outputStream = new FileOutputStream(destination);
+      //Creating a buffer to hold the data
+      int length = (int) source.length();
+      byte[] buffer = new byte[length];
+      while ((length = inputStream.read(buffer)) > 0) {
+         outputStream.write(buffer, 0, length);
+      }
+      inputStream.close();
+      outputStream.close();
+      System.out.println("File copied successfully.......");
+   }
+}
